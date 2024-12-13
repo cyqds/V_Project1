2b474b6c1b1b88095bc97ae052f72aaa8751e4b3
Yongqi.Chen <Yongqi.Chen@verisilicon.com>
2024-12-13 12:57:23 +0800

    modify build-v4l-utils.sh and add README.md

--- build-v4l-utils.sh
+++ build-v4l-utils.sh
@@ -25,14 +25,8 @@ if ! git checkout "$TAG"; then
     exit 1
 
 fi
-#install the dependencies
-sudo apt install debhelper doxygen gcc git graphviz libasound2-dev libjpeg-dev libqt5opengl5-dev libudev-dev libx11-dev meson pkg-config qtbase5-dev udev libsdl2-dev libbpf-dev llvm clang
-
-sudo apt install alsa-lib-devel doxygen libjpeg-turbo-devel qt5-qtbase-devel libudev-devel mesa-libGLU-devel
-
-sudo apt install libjson-c-dev
 
 #Building 
 meson build/
 ninja -C build/
-meson configure -Dgconv=enabled build/
+sudo ninja -C build/ install
